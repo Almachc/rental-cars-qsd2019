22f216dd37db7d8be035ac352bab7d517810d6de
Alexandre Machado <alexandre.machcruz@gmail.com>
2020-01-27 10:26:42 -0300

    WIP Implements car api validations/exceptions
    WIP Implements car api validations/exceptions

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,35 +1,55 @@
 class Api::V1::CarsController < Api::V1::ApiController
     def index
-        @cars = Car.all  
-        render json: @cars, status: :ok
+        @cars = Car.all
+        return render json: @cars, status: :ok if @cars.any?
+        render json: { notice: 'Nenhum carro encontrado' }, status: :not_found
+    rescue StandardError
+        render json: { notice: 'Ops, algo deu errado no servidor!' }, status: :internal_server_error
     end
 
     def show
         @car = Car.find(params[:id])
         render json: @car, status: :ok
+    rescue ActiveRecord::RecordNotFound
+        render json: { notice: 'Carro não encontrado' }, status: :not_found
+    rescue StandardError
+        render json: { notice: 'Ops, algo deu errado no servidor!' }, status: :internal_server_error
     end
 
     def create
         @car = Car.new(car_params)
         render json: @car, status: :created if @car.save!
+    rescue ActionController::ParameterMissing, ActiveRecord::RecordInvalid
+        render json: { notice: 'Campo(s) inválido(s)' }, status: :unprocessable_entity
+    rescue StandardError
+        render json: { notice: 'Ops, algo deu errado no servidor!' }, status: :internal_server_error
     end
 
     def update
         @car = Car.find(params[:id])
         render json: @car, status: :ok if @car.update(car_params)
+    rescue ActionController::ParameterMissing, ActiveRecord::RecordInvalid
+        render json: { notice: 'Campo(s) inválido(s)' }, status: :unprocessable_entity
+    rescue StandardError
+        render json: { notice: 'Ops, algo deu errado no servidor!' }, status: :internal_server_error
     end
 
     def destroy
         car = Car.find(params[:id])
         render json: {notice: "Carro de placa '#{car.license_plate}' deletado com sucesso"}, status: :ok if car.destroy
+    rescue ActiveRecord::RecordNotFound
+        render json: { notice: 'Carro não encontrado' }, status: :not_found
+    rescue StandardError
+        render json: { notice: 'Ops, algo deu errado no servidor!' }, status: :internal_server_error
     end
 
     def status
         @car = Car.find(params[:id])
-        if  @car.update(status: params[:status])
+        if @car.update(status: params.require(:status))
             notice = "Carro de placa '#{@car.license_plate}' alterado com sucesso. Seu status agora é: '#{@car.status_in_pt}'"
             render json: {notice: notice}, status: :ok
         end
+   
     end
 
     private
@@ -37,4 +57,6 @@ class Api::V1::CarsController < Api::V1::ApiController
     def car_params
         params.require(:car).permit(:license_plate, :color, :car_model_id, :mileage, :status)
     end
+
+
 end
--- spec/requests/car_management_spec.rb
+++ spec/requests/car_management_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Car management' do
     context '- index / list: ' do
-        it 'should be render a JSON with all cars' do
+        it 'should render a JSON with all cars' do
             #Arrange
             manufacturer = Manufacturer.create!(name: 'Fabricante1')
             car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
@@ -17,15 +17,47 @@ describe 'Car management' do
             #Assert
             converted_json = JSON.parse(response.body, symbolize_names: true)
 
-            expect(response).to have_http_status(:ok)
+            expect(response).to have_http_status(:ok) #200
             expect(converted_json[0][:license_plate]).to eq(car1.license_plate)
             expect(converted_json[1][:license_plate]).to eq(car2.license_plate)
             expect(converted_json[2][:license_plate]).to eq(car3.license_plate)
         end
+
+        it 'no car should be found' do
+            #Act
+            get api_v1_cars_path
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:not_found) #404
+            expect(converted_json[:notice]).to eq('Nenhum carro encontrado')
+        end
+
+        it 'should returned a server error (500)' do
+            #Arrange
+            manufacturer = Manufacturer.create!(name: 'Fabricante1')
+            car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
+            car_model = CarModel.create!(name: 'Modelo1', year: '2019', manufacturer: manufacturer, motorization: '50', car_category: car_category, fuel_type: 'Etanol')
+            car1 = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model, mileage: '200')
+            car2 = Car.create!(license_plate: 'ABC9999', color: 'Preto', car_model: car_model, mileage: '200')
+            car3 = Car.create!(license_plate: 'CIC3301', color: 'Bronze', car_model: car_model, mileage: '200')
+            
+            allow(Car).to receive(:all).and_raise(StandardError)
+            
+            #Act
+            get api_v1_cars_path
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:internal_server_error) #500
+            expect(converted_json[:notice]).to eq('Ops, algo deu errado no servidor!')
+        end
     end
 
     context '- show: ' do
-        it 'should be render a JSON with details of a specific car' do
+        it 'should render a JSON with details of a specific car' do
             #Arrange
             manufacturer = Manufacturer.create(name: 'Fabricante1')
             car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
@@ -45,10 +77,40 @@ describe 'Car management' do
             expect(converted_json[:mileage]).to eq(car.mileage)
             expect(converted_json[:status]).to eq(car.status)
         end
+
+        it 'no car should be found' do
+            #Act
+            get api_v1_car_path(000)
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:not_found)
+            expect(converted_json[:notice]).to eq('Carro não encontrado')
+        end
+
+        it 'should returned a server error (500)' do
+            #Arrange
+            manufacturer = Manufacturer.create(name: 'Fabricante1')
+            car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
+            car_model = CarModel.create!(name: 'Modelo1', year: '2019', manufacturer: manufacturer, motorization: '50', car_category: car_category, fuel_type: 'Etanol')
+            car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model, mileage: '200')
+
+            allow(Car).to receive(:find).and_raise(StandardError)
+
+            #Act
+            get api_v1_car_path(car)
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:internal_server_error)
+            expect(converted_json[:notice]).to eq('Ops, algo deu errado no servidor!')
+        end
     end
 
     context '- create: ' do
-        it 'should be create a car and render a JSON with its details' do
+        it 'should create a car and render a JSON with its details' do
             #Arrange
             manufacturer = Manufacturer.create(name: 'Fabricante1')
             car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
@@ -66,17 +128,57 @@ describe 'Car management' do
             expect(car.car_model_id).to eq(car_model.id)
             expect(car.mileage).to eq(200)
 
-            expect(response).to have_http_status(201)
+            expect(response).to have_http_status(:created) #201
             expect(converted_json[:license_plate]).to eq(car.license_plate)
             expect(converted_json[:color]).to eq(car.color)
             expect(converted_json[:car_model_id]).to eq(car.car_model_id)
             expect(converted_json[:mileage]).to eq(car.mileage)
             expect(converted_json[:status]).to eq(car.status)
         end
+
+        it 'should returned a client error (ActionController::ParameterMissing)' do
+            #Act
+            post api_v1_cars_path, params: { }
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:unprocessable_entity) #422
+            expect(converted_json[:notice]).to eq('Campo(s) inválido(s)')
+        end
+
+        it 'should returned a client error (ActiveRecord::RecordInvalid)' do
+            #Act
+            post api_v1_cars_path, params: { car: { license_plate: 'ABC1234' }}
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:unprocessable_entity) #422
+            expect(converted_json[:notice]).to eq('Campo(s) inválido(s)')
+        end
+
+        it 'should returned a server error (500)' do
+            #Arrange
+            manufacturer = Manufacturer.create(name: 'Fabricante1')
+            car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
+            car_model = CarModel.create!(name: 'Modelo1', year: '2019', manufacturer: manufacturer, motorization: '50', car_category: car_category, fuel_type: 'Etanol')
+
+            allow_any_instance_of(Car).to receive(:save!).and_raise(StandardError)
+
+            #Act
+            post api_v1_cars_path, params: { car: { license_plate: 'ABC1234', color: 'Branco', car_model_id: car_model.id, mileage: '200' } }
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:internal_server_error)
+            expect(converted_json[:notice]).to eq('Ops, algo deu errado no servidor!')
+        end
     end
 
     context '- update: ' do
-        it 'should be update a car and render a JSON with its details' do
+        it 'should update a car and render a JSON with its details' do
             #Arrange
             manufacturer = Manufacturer.create(name: 'Fabricante1')
             car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
@@ -97,10 +199,63 @@ describe 'Car management' do
             expect(converted_json[:car_model_id]).to eq(car.car_model_id)
             expect(converted_json[:mileage]).to eq(car.mileage)
         end
+
+        it 'should returned a client error (ActionController::ParameterMissing)' do
+            #Arrange
+            manufacturer = Manufacturer.create(name: 'Fabricante1')
+            car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
+            car_model = CarModel.create!(name: 'Modelo1', year: '2019', manufacturer: manufacturer, motorization: '50', car_category: car_category, fuel_type: 'Etanol')
+            car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model, mileage: '200')
+
+            #Act
+            patch api_v1_car_path(car), params: { }
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:unprocessable_entity) #422
+            expect(converted_json[:notice]).to eq('Campo(s) inválido(s)')
+        end
+
+        it 'should returned a client error (ActiveRecord::RecordInvalid)' do
+            #Arrange
+            manufacturer = Manufacturer.create(name: 'Fabricante1')
+            car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
+            car_model = CarModel.create!(name: 'Modelo1', year: '2019', manufacturer: manufacturer, motorization: '50', car_category: car_category, fuel_type: 'Etanol')
+            car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model, mileage: '200')
+
+            #Act
+            patch api_v1_car_path(car), params: { car: {  } }
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:unprocessable_entity) #422
+            expect(converted_json[:notice]).to eq('Campo(s) inválido(s)')
+        end
+
+        it 'should returned a server error (500)' do
+            #Arrange
+            manufacturer = Manufacturer.create(name: 'Fabricante1')
+            car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
+            car_model = CarModel.create!(name: 'Modelo1', year: '2019', manufacturer: manufacturer, motorization: '50', car_category: car_category, fuel_type: 'Etanol')
+            car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model, mileage: '200')
+
+            allow_any_instance_of(Car).to receive(:update).and_raise(StandardError)
+
+            #Act
+            patch api_v1_car_path(car), params: { car: { license_plate: 'ABC1234', color: 'Laranja', car_model_id: car_model.id, mileage: '200' } }
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:internal_server_error)
+            expect(converted_json[:notice]).to eq('Ops, algo deu errado no servidor!')
+        end
     end
 
     context '- delete: ' do
-        it 'should be delete a car and render a JSON with its details' do
+        it 'should delete a car and render a JSON with its details' do
             #Arrange
             manufacturer = Manufacturer.create(name: 'Fabricante1')
             car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
@@ -119,6 +274,36 @@ describe 'Car management' do
 
             expect(response).to have_http_status(:ok)
             expect(converted_json[:notice]).to eq("Carro de placa 'CBA1212' deletado com sucesso")
+        end
+
+        it 'no car should be found' do
+            #Act
+            delete "/api/v1/cars/#{1}"
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:not_found)
+            expect(converted_json[:notice]).to eq('Carro não encontrado')
+        end
+
+        it 'should returned a server error (500)' do
+            #Arrange
+            manufacturer = Manufacturer.create(name: 'Fabricante1')
+            car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
+            car_model = CarModel.create!(name: 'Modelo1', year: '2019', manufacturer: manufacturer, motorization: '50', car_category: car_category, fuel_type: 'Etanol')
+            car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model, mileage: '200')
+
+            allow_any_instance_of(Car).to receive(:destroy).and_raise(StandardError)
+
+            #Act
+            delete "/api/v1/cars/#{car.id}"
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:internal_server_error)
+            expect(converted_json[:notice]).to eq('Ops, algo deu errado no servidor!')
         end
     end
 
@@ -140,6 +325,42 @@ describe 'Car management' do
 
             expect(response).to have_http_status(:ok)
             expect(converted_json[:notice]).to eq("Carro de placa 'ABC1234' alterado com sucesso. Seu status agora é: 'Indisponível'")
+        end
+
+        it 'should returned a client error (ActiveRecord::RecordInvalid)' do
+            #Arrange
+            manufacturer = Manufacturer.create(name: 'Fabricante1')
+            car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
+            car_model = CarModel.create!(name: 'Modelo1', year: '2019', manufacturer: manufacturer, motorization: '50', car_category: car_category, fuel_type: 'Etanol')
+            car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model, mileage: '200', status: 'available')
+
+            #Act
+            patch status_api_v1_car_path(car), params: { status: 'seila' }
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:unprocessable_entity) #422
+            expect(converted_json[:notice]).to eq('Campo(s) inválido(s)')
+        end
+
+        it 'should returned a server error (500)' do
+            #Arrange
+            manufacturer = Manufacturer.create(name: 'Fabricante1')
+            car_category = CarCategory.create!(name: 'Categoria1', daily_rate: 1.2, car_insurance: 1.3, third_party_insurance: 1.4)
+            car_model = CarModel.create!(name: 'Modelo1', year: '2019', manufacturer: manufacturer, motorization: '50', car_category: car_category, fuel_type: 'Etanol')
+            car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model, mileage: '200', status: 'available')
+
+            allow_any_instance_of(Car).to receive(:update).and_raise(StandardError)
+
+            #Act
+            patch status_api_v1_car_path(car), params: { status: 'unavailable' }
+
+            #Assert
+            converted_json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to have_http_status(:internal_server_error)
+            expect(converted_json[:notice]).to eq('Ops, algo deu errado no servidor!')
         end
     end
 end
